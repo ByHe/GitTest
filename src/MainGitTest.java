da4a5adb311c350dda49aa71c3670773ecf63493
G-018724 <G-018724@G-018724-Dator>
2017-01-18 20:32:52 +0100

    Andra ändringen.

--- src/MainGitTest.java
+++ src/MainGitTest.java
@@ -4,7 +4,7 @@ public class MainGitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello EGit 1");
-		
+		System.out.println("Hello EGit 2");
 
 	}
 
